0c8afb54313c31965f5c1f9c0f8b4e0de9dbea49
Conor Calam <RockingEnd@gmail.com>
2021-04-22 16:18:52 +1000

    Adding TheProject.mp5
    Tom Clancy's EA Sports The "NEW!" Super Cosmic Space Game From Outer Space: In Space Starring COSMONAUTS Game of the Year Edition Gluten Free, and Knuckles Deluxe, Now Featuring Dante from DMC!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    public float moveSpeed;
+
+    [SerializeField]
+    public GameObject playerCamera;
+
+    [SerializeField]
+    public float rotateSpeed;
+
+    [SerializeField]
+    public GameObject playerMesh;
+
+    [SerializeField]
+    private int playerIndex = 0;
+
+    private CharacterController controller;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector2 inputVector = Vector2.zero;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    public int GetPlayerIndex()
+    {
+        return playerIndex;
+    }
+
+    public void SetInputVector(Vector2 direction)
+    {
+        inputVector = direction;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rotationVector = new Vector3(inputVector.x, 0, inputVector.y);
+        moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection = Quaternion.Euler(0, playerCamera.gameObject.transform.eulerAngles.y, 0) * moveDirection;
+        moveDirection *= moveSpeed;
+
+        controller.Move(moveDirection * Time.deltaTime);
+        
+        var rotation = Quaternion.LookRotation(moveDirection);
+
+        //Checks if there is still input coming from the player. This prevents the mesh from rotating back to Y = 0 when there's no input.
+        if (moveDirection.magnitude != 0)
+        {
+            RotateTowardsMovement(rotation);
+        }
+        
+    }
+
+    public void RotateTowardsMovement(Quaternion rotation)
+    {
+
+        //Rotates the Player's mesh towards the 'rotation' variable.
+        playerMesh.gameObject.transform.rotation = Quaternion.RotateTowards(playerMesh.gameObject.transform.rotation, rotation, rotateSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+
+    private PlayerInput playerInput;
+    private PlayerController playerController;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        var playerControllers = FindObjectsOfType<PlayerController>();
+        var index = playerInput.playerIndex;
+        playerController = playerControllers.FirstOrDefault(m => m.GetPlayerIndex() == index);
+        //playerController.playerCamera.SetActive(true);
+        //playerInput.camera = playerController.playerCamera.GetComponent<Camera>();
+    }
+
+    public void OnMove(CallbackContext context)
+    {
+        if(playerController != null)
+        playerController.SetInputVector(context.ReadValue<Vector2>());
+    }
+
+    private void Update()
+    {
+        gameObject.transform.position = playerController.transform.position;
+    }
+
+}
