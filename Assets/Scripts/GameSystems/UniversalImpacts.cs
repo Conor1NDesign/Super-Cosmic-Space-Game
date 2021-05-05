f4531a3db44aa3f613a8a30e542c482a704b9778
Conor Calam <RockingEnd@gmail.com>
2021-05-05 17:27:49 +1000

    Messes Spawn + Universal Impact Control (I'm gonna)

--- Assets/Scripts/GameSystems/UniversalImpacts.cs
+++ Assets/Scripts/GameSystems/UniversalImpacts.cs
@@ -11,7 +11,7 @@ public class UniversalImpacts : MonoBehaviour
     public GameObject engineeringSystem;
     public GameObject fuelStationSystem;
     public GameObject lifeSupportSystem;
-    public GameObject researchTableSystem;
+    //public GameObject researchTableSystem;
     private int selectedSystem;
 
     [Header("Max Number of Impacts")]
@@ -25,6 +25,20 @@ public class UniversalImpacts : MonoBehaviour
 
     [Header("Impact Roll Value")]
     public int diceRoll;
+
+    [Header("Mess Generation Variables")]
+    public int numberOfMessZones;
+    public GameObject messSpawnZone01;
+    public GameObject messSpawnZone02;
+    public GameObject messSpawnZone03;
+    public GameObject messSpawnZone04;
+    public GameObject messSpawnZone05;
+    public GameObject messSpawnZone06;
+    //If more spawn areas are added, put them here lmao
+    public int messSpawnChance;
+    private int selectedMessZone;
+
+
 
 
     public void Awake()
@@ -60,24 +74,49 @@ public class UniversalImpacts : MonoBehaviour
             RollTheDice(numberOfSystems);
             selectedSystem = diceRoll;
 
-            if (selectedSystem == 0)
+            if (selectedSystem == 1)
                 bridgeControlSystem.GetComponent<ShipSystems>().Impact();
 
-            if (selectedSystem == 1)
+            if (selectedSystem == 2)
                 craftingBenchSystem.GetComponent<ShipSystems>().Impact();
 
-            if (selectedSystem == 2)
+            if (selectedSystem == 3)
                 engineeringSystem.GetComponent<ShipSystems>().Impact();
 
-            if (selectedSystem == 3)
+            if (selectedSystem == 4)
                 fuelStationSystem.GetComponent<ShipSystems>().Impact();
 
-            if (selectedSystem == 4)
+            if (selectedSystem == 5)
                 lifeSupportSystem.GetComponent<ShipSystems>().Impact();
 
-            if (selectedSystem == 5)
-                researchTableSystem.GetComponent<ShipSystems>().Impact();
+            //if (selectedSystem == 5)6
+                //researchTableSystem.GetComponent<ShipSystems>().Impact();
         }
 
+        RollTheDice(101);
+
+        if(diceRoll <= messSpawnChance)
+        {
+            RollTheDice(numberOfMessZones);
+            selectedMessZone = diceRoll;
+
+            if (selectedMessZone == 1)
+                messSpawnZone01.GetComponent<MessController>().MakeAMess();
+
+            if (selectedMessZone == 2)
+                messSpawnZone02.GetComponent<MessController>().MakeAMess();
+
+            if (selectedMessZone == 3)
+                messSpawnZone03.GetComponent<MessController>().MakeAMess();
+
+            if (selectedMessZone == 4)
+                messSpawnZone04.GetComponent<MessController>().MakeAMess();
+
+            if (selectedMessZone == 5)
+                messSpawnZone05.GetComponent<MessController>().MakeAMess();
+
+            if (selectedMessZone == 6)
+                messSpawnZone06.GetComponent<MessController>().MakeAMess();
+        }
     }
 }
