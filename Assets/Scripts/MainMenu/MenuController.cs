422a6012584a005cfa876f3783029cfb565747cd
Conor Calam <RockingEnd@gmail.com>
2021-05-14 14:32:22 +1000

    Main Menu Poggers :O

---
+++ Assets/Scripts/MainMenu/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    [Header("Player Index: 0 for P1, 1 for P2, etc.")]
+    public int playerIndex;
+
+    public GameObject playerJoinIcon;
+    public GameObject playerJoinedIcon;
+    public GameObject playerRenderGreyed;
+    public GameObject playerRenderFull;
+
+    public bool activePlayer;
+
+    public int GetPlayerIndex()
+    {
+        return playerIndex;
+    }
+
+    public void UpdateJoinedUI()
+    {
+        activePlayer = true;
+        playerJoinIcon.SetActive(false);
+        playerJoinedIcon.SetActive(true);
+
+        playerRenderGreyed.SetActive(false);
+        playerRenderFull.SetActive(true);
+    }
+}
--- Assets/Scripts/Players/PlayerInputHandler.cs
+++ Assets/Scripts/Players/PlayerInputHandler.cs
@@ -7,9 +7,11 @@ using UnityEngine.InputSystem.Controls;
 using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.UI;
 using static UnityEngine.InputSystem.InputAction;
+using UnityEngine.SceneManagement;
 
 public class PlayerInputHandler : MonoBehaviour
 {
+    private MenuController menuController;
 
     private PlayerInput playerInput;
     private PlayerController playerController;
@@ -19,10 +21,28 @@ public class PlayerInputHandler : MonoBehaviour
     private GameObject playerCanvas;
     private GameObject playerCamera;
 
+    private MenuButtons startButtonObject;
+    private MenuButtons quitPromptObject;
+    private MenuButtons quitConfirmObject;
+
     private float playerMoveSpeed;
 
     private void Awake()
     {
+        DontDestroyOnLoad(gameObject);
+
+        playerInput = GetComponent<PlayerInput>();
+
+        var menuControllers = FindObjectsOfType<MenuController>();
+        var index = playerInput.playerIndex;
+        menuController = menuControllers.FirstOrDefault(m => m.GetPlayerIndex() == index);
+        menuController.UpdateJoinedUI();
+
+        startButtonObject = GameObject.Find("PlayButton").GetComponent<MenuButtons>();
+        quitPromptObject = GameObject.Find("QuitPromptButton").GetComponent<MenuButtons>();
+
+
+        /* COMMENTED CHUNK OF AWAKE METHOD OUT FOR MAIN MENU TESTING
         playerInput = GetComponent<PlayerInput>();
         var playerControllers = FindObjectsOfType<PlayerController>();
         var index = playerInput.playerIndex;
@@ -54,7 +74,7 @@ public class PlayerInputHandler : MonoBehaviour
 
         playerCanvas.GetComponent<Canvas>().worldCamera = playerCamera.GetComponent<Camera>();
         playerCanvas.GetComponent<Canvas>().planeDistance = 1;
-
+        */
     }
 
     public void OnMove(CallbackContext context)
@@ -73,6 +93,11 @@ public class PlayerInputHandler : MonoBehaviour
                 playerController.RecieveButtonInput(button);
             }
         }
+
+        else if (quitConfirmObject != null)
+        {
+            quitConfirmObject.QuitConfirm();
+        }
     }
 
     public void OnBButtonPush(CallbackContext context)
@@ -84,6 +109,17 @@ public class PlayerInputHandler : MonoBehaviour
                 var button = ShipSystems.buttonOptions.BButton;
                 playerController.RecieveButtonInput(button);
             }
+        }
+        else if (quitConfirmObject != null)
+        {
+            quitConfirmObject.CancelQuitting();
+            quitConfirmObject = null;
+        }
+
+        else if (quitPromptObject != null)
+        {
+            quitPromptObject.QuitPrompt();
+            quitConfirmObject = quitPromptObject.exitPrompt.GetComponent<MenuButtons>();
         }
     }
 
@@ -153,12 +189,65 @@ public class PlayerInputHandler : MonoBehaviour
         }
     }
 
+    public void OnStartButtonPress(CallbackContext context)
+    {
+        if (startButtonObject != null)
+        {
+            if (context.performed)
+                startButtonObject.PlayTheGame();
+        }
+    }
+
 
 
     public void Update()
     {
+        if (playerController != null)
         gameObject.transform.position = playerController.transform.position;
+
+        if (playerController == null && SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Main_ShipScene"))
+        {
+            OnSceneLoaded();
+        }
     }
 
-  
+
+    public void OnSceneLoaded()
+    {
+        Debug.Log("Hey this works!");
+        startButtonObject = null;
+        quitPromptObject = null;
+
+        var playerControllers = FindObjectsOfType<PlayerController>();
+        var index = playerInput.playerIndex;
+        playerController = playerControllers.FirstOrDefault(m => m.GetPlayerIndex() == index);
+        playerMoveSpeed = playerController.moveSpeed;
+        inventoryManager = playerController.GetComponent<InventoryManager>();
+
+        playerCamera = transform.Find("Camera").gameObject;
+        playerCamera.SetActive(true);
+
+        if (playerController.playerIndex == 0)
+        {
+            playerCanvas = GameObject.Find("Canvas_P1");
+        }
+
+        if (playerController.playerIndex == 1)
+        {
+            playerCanvas = GameObject.Find("Canvas_P2");
+        }
+
+        if (playerController.playerIndex == 2)
+        {
+            playerCanvas = GameObject.Find("Canvas_P3");
+        }
+
+        if (playerController.playerIndex == 3)
+        {
+            playerCanvas = GameObject.Find("Canvas_P4");
+        }
+
+        playerCanvas.GetComponent<Canvas>().worldCamera = playerCamera.GetComponent<Camera>();
+        playerCanvas.GetComponent<Canvas>().planeDistance = 1;
+    }
 }
